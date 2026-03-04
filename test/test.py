5ed18a98d36cdd7a7011835f762e1f1fa9437d4e
Nicholas Gaither <nicholas.gaither@gmail.com>
2026-03-03 17:52:39 -0800

    Refactor test by removing clock cycle delays
    Removed unnecessary clock cycle waits in test.

--- test/test.py
+++ test/test.py
@@ -20,13 +20,9 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
     dut._log.info("CocoTB testbench")
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 5)
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
